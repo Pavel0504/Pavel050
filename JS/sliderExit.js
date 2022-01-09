43ee2edcbf4bc3c5554e3cc3731966c7fa61f99d
Pavel0504 <87333895+Pavel0504@users.noreply.github.com>
2022-01-09 04:35:19 +0300

    step trhee

---
+++ JS/sliderExit.js
@@ -0,0 +1,8 @@
+const exit = document.querySelectorAll(".exit");
+const containers = document.querySelector(".container");
+
+exit.forEach((exit) =>
+    exit.addEventListener("click", () => {
+        containers.classList.toggle("active");
+    })
+);
