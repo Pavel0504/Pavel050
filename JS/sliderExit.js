498ff820e0209cfe22dba1a9fa603c9a69d4d2b5
Pavel0504 <87333895+Pavel0504@users.noreply.github.com>
2022-01-10 20:28:15 +0300

    fix links slider

--- JS/sliderExit.js
+++ JS/sliderExit.js
@@ -2,7 +2,8 @@ const exit = document.querySelectorAll(".exit");
 const containers = document.querySelector(".container");
 
 exit.forEach((exit) =>
-    exit.addEventListener("click", () => {
-        containers.classList.toggle("active");
-    })
+  exit.addEventListener("click", () => {
+    containers.classList.toggle("active");
+    linkv.style.display = "flex";
+  })
 );
